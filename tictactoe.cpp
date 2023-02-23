e7a29a9b403e86d97895ba043edc45b3c17cd1ca
Paul Norling <paulnorling87@gmail.com>
2023-02-23 10:16:43 -0600

    this is too much fun

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,19 +2,27 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-//Array for the board
+
+//Array of the board
 char board[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
 
 //variables
 int choice;
-bool gameOn= true;
-int row, column;
+bool gameOn = true;
+int row;
+int column;
+char turn = 'X';
 
-
+//display board
+void display_board() {
+    cout<<board[0][0]<<'|'<<board[0][1]<<'|'<<board[0][2]<<endl;
+    cout<<board[1][0]<<'|'<<board[1][1]<<'|'<<board[1][2]<<endl;
+    cout<<board[2][0]<<'|'<<board[2][1]<<'|'<<board[2][2]<<endl;
+}
 
 //take in choice
 void selection() {
-    cout<<"make a selction \n";
+    cout<<"Player "<<turn<<" "<<"make a selction \n";
     cin>> choice;
    switch(choice) {
     case 1: row=0; column=0; break;
@@ -29,16 +37,41 @@ void selection() {
     default:
         cout<<"invalid selection \n";
    }
-   if(choice==1) {
+   if(turn=='X' && board[row][column]!='X' && board[row][column]!='O') {
    board[row][column]='X';
+   turn='O';
+   }else if(turn=='O' && board[row][column]!='X' && board[row][column]!='O') {
+   board[row][column]='O';
+   turn='X';
+   }else {
+    cout<<"Choose another box \n";
    }
-   cout<<board[0][0]<<endl;
 }
-
+//check for win
+void gameWon() {
+    for(int i; i<3; i++){
+        //row win
+        if(board[i][0]==board[i][1] && board[i][1]==board[i][2]){
+            cout<<"Win!!";
+            gameOn = false;
+        //column win
+        }else if(board[0][i]==board[1][i] && board[1][i]==board[2][i]){
+            cout<<"Win!!";
+            gameOn = false;
+        //diagonal win
+        }else if(board[0][0]==board[1][1] && board[1][1]==board[2][2] || board[0][2]==board[1][1] && board[1][1]==board[2][0]){
+            cout<<"Win!!";
+            gameOn = false;
+        }
+    }    
+}
+//check for draw
 int main(){
     cout<<"tictactoe \n";
     while(gameOn) {
+        display_board();
         selection();
+        gameWon();
     }
     return 0;
 }
